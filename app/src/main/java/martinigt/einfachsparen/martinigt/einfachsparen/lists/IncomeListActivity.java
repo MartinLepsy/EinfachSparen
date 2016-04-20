cfa2e9a12ff40b3774a414a8eec91894ed86a3de
Martin <Martinlepsy@gmx.de>
2016-04-20 19:15:05 +0200

    TEst vom Laptop

--- app/src/main/java/martinigt/einfachsparen/martinigt/einfachsparen/lists/IncomeListActivity.java
+++ app/src/main/java/martinigt/einfachsparen/martinigt/einfachsparen/lists/IncomeListActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
 import martinigt.einfachsparen.R;
 import martinigt.einfachsparen.forms.CreateIncomeActivity;
 
 public class IncomeListActivity extends AppCompatActivity {
+
+    private ListView incomeListView;
+
+    private FloatingActionButton addIncomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +26,18 @@ public class IncomeListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getReferencesToWidgets();
+
+        bindListeners();
+    }
+
+    private void getReferencesToWidgets() {
+        addIncomeButton = (FloatingActionButton) findViewById(R.id.fab);
+        incomeListView = (ListView) findViewById(R.id.incomeList);
+    }
+
+    private void bindListeners() {
+        addIncomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent goToCreateIncome = new Intent(getApplicationContext(),
